9d84cb3c8513dca29731456eeec2b6e765c21e14
Edward J. Schwartz <eschwartz@cert.org>
2015-03-03 10:11:18 -0500

    Oops, workers consume, not produce

--- worker.bash
+++ worker.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-screen -S autoexp bash -c "./autoexp_producer.py --server 127.0.0.1 2>&1 | tee /tmp/log"
+screen -S autoexp bash -c "./autoexp_consumer.py --server 127.0.0.1 2>&1 | tee /tmp/log"
 sudo poweroff
 exit 0
