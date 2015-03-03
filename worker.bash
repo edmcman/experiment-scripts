d9a824f7cd4308d830cc07e6f2b9fb3a22f8d90e
Edward J. Schwartz <eschwartz@cert.org>
2015-03-03 10:57:34 -0500

    Add sleep before powering off

--- worker.bash
+++ worker.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+SLEEP=60
+
 screen -S autoexp bash -c "./autoexp_consumer.py --server 127.0.0.1 2>&1 | tee /tmp/log"
+echo Powering off in $SLEEP seconds
+sleep $SLEEP
 sudo poweroff
 exit 0
